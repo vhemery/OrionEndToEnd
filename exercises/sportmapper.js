f034aacff68e6a5156c185ca2f83962c2ecf083b
vhemery <vincent.hemery@c-s.fr>
2014-06-18 06:16:04 -0400

    End of exercise 2

---
+++ exercises/sportmapper.js
@@ -0,0 +1,52 @@
+/*global google Tabletop*/
+/*jslint browser:true*/
+
+
+/**
+ * Build content information about a single team on a map
+ * @param {Object} team A single team from the Bundesliga database
+ * 
+ */
+function buildContent( data ){
+
+	var twitterLogo = "	https://abs.twimg.com/a/1382598364/images/resources/twitter-bird-blue-on-white.png";
+	
+	var contentString = '<table class="table table-bordered">' +
+							'<tbody>' +
+								'<tr>' + 
+									'<th>Team</th>' + 
+									'<td><a href="' + data.website + '"</a>' + data.team + '</td>' +
+									'<td align="center"><a href="https://twitter.com/' + data.twitter + '"</a><img src=' + twitterLogo + ' alt="Twitter" height="20" width="20"</td>' + 
+								'</tr>' +
+								'<tr><th>Stadium</th><td colspan="2">' + data.stadium + '</td></tr>' +
+								'<tr><th>Capacity</th><td colspan="2">' + data.capacity + '</td></tr>' +
+								'<tr><th>Address</th><td colspan="2">' + data.address + '</td></tr>' +
+							'</tbody>' +
+						'<table>';
+	
+	return contentString;
+}
+
+function append( data ){
+	var container = document.getElementById( 'tables' );	
+	var wrapper = document.createElement( 'div' );	
+	var content = buildContent( data );	
+	wrapper.innerHTML = content;
+	
+	container.appendChild( wrapper );
+}
+
+function showInfo(data) {	
+    data.teams.elements.forEach( append );
+}
+
+window.onload = function() {
+   var spreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=0Ah39DYKnfRHedDlxMmpaaVBuU21WVGliTjUyV3YwRkE&usp=sharing&output=html';
+    
+    //uncomment sheet below for Canadian version
+    //var spreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=0AnDA54eMM-5ydEJVaGNhaXR3d2RDblJ6ZEdfU3A0UXc&usp=sharing&output=html';
+    //uncomment sheet below for German version
+    //var spreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=0AhLgoEUzhCg_dEFKOS1kUG5iNDJrc2dSVGg0dWZBekE&usp=sharing&output=html';
+    
+    Tabletop.init({ key: spreadsheet, callback: showInfo });
+};
